51312ab6ea4b516a1b6ba4e2d8b6df09528b16b8
steve <meadsteve@gmail.com>
2020-03-16 17:01:01 +0100

    express typescript skeleton

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+const port : string|number= process.env.PORT || 5000;
+
+app.use("*",(req, res) =>{res.send("<h1>Welcome to your server!</h1>")});
+//create a server object:
+app.listen(port,() => console.log(`hosting @${port}`));
+
